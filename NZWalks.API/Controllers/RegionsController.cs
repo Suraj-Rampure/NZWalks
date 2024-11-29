1dd4ca1d5e9498e60b931f6838a8248d2081d1f5
Suraj-Rampure <rampuresuraj26@gmail.com>
2024-11-30 01:53:26 +0530

    Lecture 68 Role based Authorization

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -15,7 +15,6 @@ namespace NZWalks.API.Controllers
     //https://localhost:1234/api/regions
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
     public class RegionsController : ControllerBase
     {
         private readonly NZWalksDbContext dbContext;
@@ -33,6 +32,7 @@ namespace NZWalks.API.Controllers
         // GET All Regions
         //GET : https://localhost:portnumber/api/regions
         [HttpGet]
+        [Authorize(Roles ="Reader")]
         public async Task<IActionResult> GetAll()
         {
             //Get Data from Database - Domain Models
@@ -48,6 +48,7 @@ namespace NZWalks.API.Controllers
         //GET : https://localhost:portnumber/api/regions/{id}
         [HttpGet]
         [Route("{id:Guid}")]
+        [Authorize(Roles = "Reader")]
         public async Task<IActionResult> GetById([FromRoute] Guid id)
         {
            
@@ -72,6 +73,7 @@ namespace NZWalks.API.Controllers
         //POST: https://localhost:portnumber/api/regions
         [HttpPost]
         [ValidateModel]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> Create([FromBody] AddRegionRequestDto addRegionRequestDto)
         {
             
@@ -95,6 +97,7 @@ namespace NZWalks.API.Controllers
         [HttpPut]
         [Route("{id:Guid}")]
         [ValidateModel]
+        [Authorize(Roles = "Writer")]
         public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
         {
            
@@ -124,7 +127,7 @@ namespace NZWalks.API.Controllers
         //DELETE : https://localhost:portnumber/api/regions/{id}
         [HttpDelete]
         [Route("{id=Guid}")]
-
+        [Authorize(Roles = "Writer,Reader")]
         public async Task<IActionResult> Delete([FromRoute] Guid id)
         {
             var regionDomainModel = await regionRepository.DeleteAsync(id);
